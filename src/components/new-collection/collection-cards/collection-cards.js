070e3cc194f7c7e8feaf95308c676a129eb8585d
Amitya31 <amitya1576@gmail.com>
2025-01-18 12:25:16 +0530

    completed all tasks

--- src/components/new-collection/collection-cards/collection-cards.js
+++ src/components/new-collection/collection-cards/collection-cards.js
@@ -1,21 +1,30 @@
+import { NavLink } from "react-router";
 import "./collection-cards.css";
 
-const Cards = ({ image, title, sub_title, price }) => {
+const Cards = ({ id, image, title, sub_title, price }) => {
   return (
-    <div className="card">
-      <div
-        className="card-image"
-        style={{
-          background: "url('" + image + "')",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-      ></div>
-      <div className="card-title">{title}</div>
-      <div className="card-sub">{sub_title}</div>
-      <div className="card-price">${price}</div>
-    </div>
+    <NavLink to={`/accessories/${id}`}
+      state={{id, image, title,sub_title,price}}
+      style={{
+        textDecoration: "none",
+        color: "black",
+      }}
+    >
+      <div className="card">
+        <div
+          className="card-image"
+          style={{
+            background: "url('" + image + "')",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="card-title">{title}</div>
+        <div className="card-sub">{sub_title}</div>
+        <div className="card-price">${price}</div>
+      </div>
+    </NavLink>
   );
 };
 
